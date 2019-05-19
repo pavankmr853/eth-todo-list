d304da07439d1d2c5840ac0dd6b21c4c32e56220
pavankmr853 <pavankmr853@gmail.com>
2019-05-19 13:37:04 +0530

    2. List tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,22 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-uint public taskCount = 0;
+  uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+mapping(uint => Task) public tasks;
+
+constructor() public {
+       createTask("For quires ping me in https://www.instagram.com/pardhu_son_of_pandu_garu/");
+     }
+
+function createTask(string memory _content) public {
+  taskCount ++;
+  tasks[taskCount] = Task(taskCount, _content, false);
+}
 }
